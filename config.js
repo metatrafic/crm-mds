8c7bc593ee36ac80ef2990a89ecb3f3c3b28b5aa
metatrafic <metatrafic@gmail.com>
2026-07-10 12:50:51 +0200

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.CRM_MDS_CONFIG = {
-  supabaseUrl: 'https://rbnqadydpaqosxmojkms.supabase.co'
-',
+  supabaseUrl: 'https://rbnqadydpaqosxmojkms.supabase.co',
   supabaseAnonKey: 'sb_publishable_-kXvmXIDTk2-0GR0AJyVGw_j5cM9738'
 };
